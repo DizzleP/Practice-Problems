20436f2d9649e6974aeacd101c9d5abadbc4d1d6
DizzleP <daniel.piasecki@bucks.edu>
2016-09-02 10:47:24 -0400

    Fizzbuzz List

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,11 +1,12 @@
 for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
-        print('Fizzbuzz')
-    elif i % 3 == 0:
-        print('Fizz')
-    elif i % 5 == 0:
-        print('Buzz')
-    else:
+    fizz = list(range(3,101,3))
+    buzz = list(range(5,101,5))
+    string = ''
+    if i not in fizz and i not in buzz:
         print(i)
-
-
+    if i in fizz:
+        string += 'Fizz'
+    if i in buzz:
+        string += 'Buzz'
+    if string != '':
+        print(string)
